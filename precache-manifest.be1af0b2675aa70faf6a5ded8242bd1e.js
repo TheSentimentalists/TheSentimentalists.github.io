580476371fd7a3a6fb6bef820628b6f261209ac6
jsjohnstone <jsjohnstone@users.noreply.github.com>
2020-10-24 16:25:43 +0000

    deploy: TheSentimentalists/SentimentalistsApp-Frontend@c591711c5345224624fe1dcaeec496be2d423183

--- precache-manifest.be1af0b2675aa70faf6a5ded8242bd1e.js
+++ precache-manifest.be1af0b2675aa70faf6a5ded8242bd1e.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7151f86d05fe7d70d138497515ea3a31",
+    "revision": "18f5edeae4e5b4579fe86003299b7a5b",
     "url": "/index.html"
   },
   {
-    "revision": "be4f8f68a7fea15e2792",
+    "revision": "637b6def829ca062b86e",
     "url": "/static/css/2.156fe21f.chunk.css"
   },
   {
-    "revision": "1260f3e8e8820df80688",
+    "revision": "b0c6635b7ff115b2a097",
     "url": "/static/css/main.75004308.chunk.css"
   },
   {
-    "revision": "be4f8f68a7fea15e2792",
-    "url": "/static/js/2.125d4aba.chunk.js"
+    "revision": "637b6def829ca062b86e",
+    "url": "/static/js/2.8cf3fe66.chunk.js"
   },
   {
     "revision": "68ba00d5eb083746d913686923a3d904",
-    "url": "/static/js/2.125d4aba.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.8cf3fe66.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1260f3e8e8820df80688",
-    "url": "/static/js/main.761c72e0.chunk.js"
+    "revision": "b0c6635b7ff115b2a097",
+    "url": "/static/js/main.df116577.chunk.js"
   },
   {
     "revision": "b5d64e5f5cee1075deb1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2117ae88a2022da9aca523e72eeee1df.js"
+  "/precache-manifest.be1af0b2675aa70faf6a5ded8242bd1e.js"
 );
 
 self.addEventListener('message', (event) => {
