d251916b2950eda1ba9aaae0eaa47cedc1e1fb20
jsjohnstone <jsjohnstone@users.noreply.github.com>
2020-10-27 11:55:52 +0000

    deploy: TheSentimentalists/SentimentalistsApp-Frontend@63fb2f70dfc4b8d2b4caaf599f4bc97f085c86a9

---
+++ precache-manifest.59780c5ac65daf7b27dce42d13e73ed9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce4ff26694b356c0419e1f7a726fab3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32db6c5c33bbea668fe1",
+    "url": "/static/css/2.af07c35f.chunk.css"
+  },
+  {
+    "revision": "a4fc6bd50224a52a9026",
+    "url": "/static/css/main.a563d430.chunk.css"
+  },
+  {
+    "revision": "32db6c5c33bbea668fe1",
+    "url": "/static/js/2.838c74ec.chunk.js"
+  },
+  {
+    "revision": "856e22d3e3764738c36089f879e82a04",
+    "url": "/static/js/2.838c74ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4fc6bd50224a52a9026",
+    "url": "/static/js/main.92ab498f.chunk.js"
+  },
+  {
+    "revision": "b5d64e5f5cee1075deb1",
+    "url": "/static/js/runtime-main.9d2c5ac2.js"
+  },
+  {
+    "revision": "b29c69c51d9ef9a933ebea32dc4d2bbf",
+    "url": "/static/media/Sentimentalists_Logo_270X273.b29c69c5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5d29dd3235e90d19423972705a7698a6.js"
+  "/precache-manifest.59780c5ac65daf7b27dce42d13e73ed9.js"
 );
 
 self.addEventListener('message', (event) => {
